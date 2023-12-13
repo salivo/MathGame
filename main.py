90bfe64d485ad8e7a959a0b832fb02ffe11bc8f8
salivo <andrijsalivon@gmail.com>
2023-12-13 15:32:39 +0100

    all work

--- main.py
+++ main.py
@@ -5,4 +5,4 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     print(url_for("index", _external=True))
-    return render_template("index.html", hostpath=(url_for("index", _external=True)), question="-5(x + 30) + 10(-6 + 4x) =", answer="= []5x []30 []60 []40")
+    return render_template("index.html", hostpath=(url_for("index", _external=True)), question="-5*(x + 30) + 10*(-6 + 4x)", answer="= []5x []30 []60 []40")
--- static/scripts/main.js
+++ static/scripts/main.js
@@ -1,9 +1,17 @@
 const math_texts = [
-  { title: '__', isInput: true, id: 1 },
-  { title: 'x', isInput: false, id: 2 },
-  { title: '___', isInput: true, id: 3 },
+  { title: '', isInput: true, id: "1" },
+  { title: '5*x', isInput: false, id: "2" },
+  { title: '', isInput: true, id: "3" },
+  { title: '150', isInput: false, id: "4" },
+  { title: '', isInput: true, id: "5" },
+  { title: '60', isInput: false, id: "6" },
+  { title: '', isInput: true, id: "7" },
+  { title: '40*x', isInput: false, id: "8" },
 ];
 
+function canc(str) {
+  return eval(str)
+}
 
 function Plus() {
     return (
@@ -25,19 +33,59 @@ function Text({text}){
     return <h1>{text}</h1>;
 }
 
+
+function removeXY(math_str){
+  let math_str_with_val = "";
+  for (let i = 0; i < math_str.length; i++) {
+    
+    const char = math_str[i];
+    if (char === "x"){
+      math_str_with_val += "2";
+    }
+    else{
+      math_str_with_val += char;
+    }
+  }
+  return math_str_with_val
+}
+
+
+function doMath(){
+  var math_str = "";
+  for (let i = 0; i< math_texts.length; i++) {
+    const element = math_texts[i];
+    if (element.isInput) {
+      var val = document.getElementById("input_" + element.id).value;
+      math_str += val
+    }
+    else{
+      math_str += element.title;
+    }
+  }
+  
+  console.log(removeXY(math_str));
+  console.log(canc(removeXY(math_str)))
+  console.log(canc(removeXY(question)));
+  return math_str;
+}
+
+
 function Math(){
   const listItems = math_texts.map(math_text =>
-    <form key={math_text.id}>
+    <div key={"div_"+math_text.id}>
       {math_text.isInput ? (
-        <input type="text" name="name" />
+        <input type="text" size="1" name="name" maxLength="1" className="answer_input" key={"input_"+math_text.id} id={"input_"+math_text.id}/>
       ) : (
-        <h3 key={math_text.id}>{math_text.title}</h3>
+        <h3 key={"text_"+math_text.id}>{math_text.title}</h3>
       )}
-    </form>
+    </div>
   );
 
   return (
-    <>{listItems}</>
+    <div className="answer_div">
+      {listItems}
+      <button onClick={doMath} key="answer_button" >answer</button>
+    </div>
   );
 }
 
